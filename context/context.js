1925c1c84f36f4cc788c5a3c59c78e7c9f7df2e5
Gaurav-Krishna-Gaali <your_email_address@example.comcls>
2022-09-11 12:47:43 +0530

    Cheggit .👻

--- context/context.js
+++ context/context.js
@@ -1,64 +1,46 @@
 //Import Dependencies and hooks needed for app
-import { createContext } from 'react'
+import { createContext } from "react";
 import {
   useVote,
   useToken,
   useAddress,
   useMetamask,
   useDisconnect,
-} from '@thirdweb-dev/react'
-import { VoteType } from '@thirdweb-dev/sdk'
-import { ethers } from 'ethers'
+} from "@thirdweb-dev/react";
+import { VoteType } from "@thirdweb-dev/sdk";
+import { ethers } from "ethers";
 
-export const ApeDaoContext = createContext()
+export const ApeDaoContext = createContext();
 export const ApeDaoProvider = ({ children }) => {
-
   /*
     Step 1. Get User address using thirdwebs hook
     Step 2. Get Token and vote contract instances using thirdwebs hooks
     Step 3. We need way to connect and disconnect from the dapp. 
   */
-  const address = true //Get the address using thirdwebs convenient hooks
+  const address = true; //Get the address using thirdwebs convenient hooks
 
-  let vote = ''
-  let token = ''
-  let connectWithMetamask = '';
-  let disconnectWallet = '';
-
-
-
+  let vote = "";
+  let token = "";
+  let connectWithMetamask = "";
+  let disconnectWallet = "";
 
   //Get all the proposals in the contract
-  const getAllProposals = async () => {
-
-  }
+  const getAllProposals = async () => {};
 
   //Check if proposal given is executable
-  const isExecutable = async id => {
-
-  }
+  const isExecutable = async (id) => {};
 
   //Check if the user has voted for the given proposal
-  const checkIfVoted = async id => {
-
-  }
+  const checkIfVoted = async (id) => {};
 
   //Create  proposal to mint tokens to the DAO's treasury
-  const createProposal = async description => {
-
-  }
-
+  const createProposal = async (description) => {};
 
   //Execute proposal if the proposal is successful
-  const executeProposal = async id => {
+  const executeProposal = async (id) => {};
 
-  }
-
-
-  //Vote for the proposal and delegate tokens if not already done. 
-  const voteFor = async (id, type, reason) => {
-
-  }
+  //Vote for the proposal and delegate tokens if not already done.
+  const voteFor = async (id, type, reason) => {};
   return (
     <ApeDaoContext.Provider
       value={{
@@ -70,10 +52,9 @@ export const ApeDaoProvider = ({ children }) => {
         connectWithMetamask,
         disconnectWallet,
         executeProposal,
-
       }}
     >
       {children}
     </ApeDaoContext.Provider>
-  )
-}
+  );
+};
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,13 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { ApeDaoProvider } from "../context/context";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ApeDaoProvider>
+      <Component {...pageProps} />;
+    </ApeDaoProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
